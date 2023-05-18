e68f93019979f4e5e2cf899030f0b10947d0a803
eduardoamorim1992 <69988624+eduardoamorim1992@users.noreply.github.com>
2023-05-18 17:41:12 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,26 @@
+const botaoAlterarTema = document.getElementById("botao-alterar-tema");
+
+const body = document.querySelector("body");
+
+const imagemBotaoTrocadeTema = document.querySelector(".imagem-botao");
+
+botaoAlterarTema.addEventListener("click", () => {
+
+    const modoEscuroEstaAtivo = body.classList.contains("modo-escuro");
+
+    if (modoEscuroEstaAtivo) {
+
+
+        body.classList.remove("modo-escuro");
+
+        imagemBotaoTrocadeTema.setAttribute("src", "./src/imagens/sun.png");
+
+
+    } else {
+
+        body.classList.add("modo-escuro");
+
+        imagemBotaoTrocadeTema.setAttribute("src", "./src/imagens/moon.png")
+    }
+
+});
